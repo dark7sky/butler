5cc0e2b9d7a5e681599878897b98c519b6729ff8
dark7sky <dark7sky@gmail.com>
2026-03-17 00:31:17 +0900

    feat: add flutter web image pipeline and hide debug banner

--- ta_watchdog/lib/main.dart
+++ ta_watchdog/lib/main.dart
@@ -19,6 +19,7 @@ class WatchDogApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Total Account',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
